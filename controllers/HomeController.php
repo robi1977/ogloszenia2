c91755879cae07e9edaeb27afde16bfb6c5ae76b
Rafal Nowak <rafal.robi.nowak@gmail.com>
2020-06-18 13:01:32 +0200

    pierwszy kontroler wyswietlajace strone home

---
+++ controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php 
+class HomeController extends Controller{
+    
+    protected function getName(){
+        return 'home';
+    }
+
+    protected function Index(){
+        //$model = new Home();
+        $this->returnView('index');
+    }
+}
