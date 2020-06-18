a9f0c6f4e930702abe36f8ec17e81f5ed933fcaf
Rafal Nowak <rafal.robi.nowak@gmail.com>
2020-06-18 15:45:55 +0200

    wprowadzenie stylowania Bootstrap3 i dopracowanie wyniku

--- views/home/index.php
+++ views/home/index.php
@@ -1,2 +1,5 @@
-<h1>Widok z pliku ./views/home/home.php</h1>
-<h2>Osadzony w pliku ./views/main.php</h2>
+<div class="text-center">
+    <h1>Widok z pliku ./views/home/home.php</h1>
+    <p class="lead">Osadzony w pliku ./views/main.php</p>
+    <a class="btn btn-primary text-center" href="<?php echo ROOT_URL; ?>ads">Zobacz</a>
+</div>
--- views/main.php
+++ views/main.php
@@ -4,9 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+
     <title>Ogłoszenia</title>
 </head>
 <body>
-    <?php require($view); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-6 col-md-offset-3">
+                <?php require($view); ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
